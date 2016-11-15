174d586aeeabd5f14e637a811d2179369d44d6c9
Ben Hubbard <harmfulmonk@gmail.com>
2016-11-15 01:38:00 -0500

    Initial Release

---
+++ UrlOnJoin/UrlOnJoin/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Core.Plugins;
+using Steamworks;
+using UnityEngine;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using Rocket.API.Collections;
+using System.Timers;
+using Rocket.Unturned.Events;
+using Rocket.Unturned;
+using SDG.Unturned;
+
+namespace UrlOnJoin
+{
+    public class Plugin : RocketPlugin<PluginConfiguration>
+    {
+        private static Plugin Instance;
+        protected override void Load()
+        {
+            Instance = this;
+            U.Events.OnPlayerConnected += Events_OnPlayerConnected;
+        }
+
+        private void Events_OnPlayerConnected(UnturnedPlayer player)
+        {
+            player.Player.channel.send("askBrowserRequest", player.CSteamID, ESteamPacket.UPDATE_RELIABLE_BUFFER, Configuration.Instance.description, Configuration.Instance.url);
+        }
+
+        protected override void Unload()
+        {
+            U.Events.OnPlayerConnected -= Events_OnPlayerConnected;
+        }
+
+        #region translations
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList() {
+                    {"",""},
+                };
+            }
+        }
+        #endregion
+    }
+}
---
+++ UrlOnJoin/UrlOnJoin/PluginConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using System.Xml.Serialization;
+using UnityEngine;
+using Steamworks;
+
+namespace UrlOnJoin
+{
+    public class PluginConfiguration : IRocketPluginConfiguration
+    {
+        public bool enabled = true;
+        public string description;
+        public string url;
+        public void LoadDefaults()
+        {
+            enabled = true;
+            description = "Welcome to my server! Please check out our website!";
+            url = "http://rawrfuls.com";
+        }
+    }
+}
